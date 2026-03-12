a89af4025aa2a855d88c9b8a5b64d5ad8a0b7556
Bryan Suquillo <brsume@alumnatflorida.es>
2026-03-12 19:08:16 +0100

    Cambio en userModel. Modifico age por birthdate

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    age: { type: Number, required: true },
+    birthdate: { type: String },
     gender: { type: String, required: true },
   },
   { timestamps: true }
